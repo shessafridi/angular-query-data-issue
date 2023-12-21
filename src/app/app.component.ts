b4bf3dbe0004622a31f48bb253f4d304d55e3f13
shessafridi <mshesskhan@gmail.com>
2023-12-21 06:14:01 +0500

    Minimum reproduction of the issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,61 @@
-import { Component } from '@angular/core';
+import { format, sub } from 'date-fns';
+
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, effect, inject, signal } from '@angular/core';
+import {
+  FormsModule,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { AngularQueryDevtools } from '@tanstack/angular-query-devtools-experimental';
+import { injectQuery } from '@tanstack/angular-query-experimental';
+
+const sleep = (ms: number) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularQueryDevtools,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-query-data-issue';
+  private fb = inject(NonNullableFormBuilder);
+
+  filters = this.fb.group({
+    startDate: [format(sub(new Date(), { days: 1 }), 'yyyy-MM-dd')],
+    endDate: [format(new Date(), 'yyyy-MM-dd')],
+  });
+
+  searchSet = signal<null | { startDate: string; endDate: string }>(null);
+
+  dashboardQuery = injectQuery(() => ({
+    queryKey: ['dashboard', this.searchSet()] as const,
+    queryFn: async ({ queryKey }) => {
+      await sleep(2000);
+      return `Your data for ${queryKey[1]?.startDate} - ${queryKey[1]?.endDate}`;
+    },
+    enabled: !!this.searchSet(),
+    staleTime: 1000 * 60 * 10,
+  }));
+
+  constructor() {
+    effect(() => {
+      const data = this.dashboardQuery.data();
+      console.log(data, 'QUERY DATA CHANGE');
+    });
+  }
+
+  search() {
+    const filters = this.filters.getRawValue();
+
+    this.searchSet.set(filters);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import {
+  provideAngularQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideAngularQuery(new QueryClient())],
 };
